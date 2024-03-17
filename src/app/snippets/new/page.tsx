a0305a59c52fc24f6bf185acd0a15b546cc7259b
TimJoePat <TimJoePat88@cox.net>
2024-03-17 05:25:25 -0700

    22 Server Action complete

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,6 +1,30 @@
+import { redirect } from "next/navigation";
+import { db } from "@/db";
+
 export default function SnippetCreatePage() {
+  async function createSnippet(formData: FormData) {
+    //This is a server action
+    "use server";
+
+    //Check user input
+    const title = formData.get("title") as string;
+    const code = formData.get("code") as string;
+
+    //Create new record in ddbase
+    const snippet = await db.snippets.create({
+      data: {
+        title,
+        code,
+      },
+    });
+    console.log(snippet);
+
+    // Redirect fo root route
+    redirect("/");
+  }
+
   return (
-    <form>
+    <form action={createSnippet}>
       <h3 className="font-bold m-3">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
